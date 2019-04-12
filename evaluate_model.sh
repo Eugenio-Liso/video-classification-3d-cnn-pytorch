b9bbf61a38f3ea05a7736bdb96f5585371739612
Eugenio <eugenio.liso@gmail.com>
2019-04-12 19:48:51 +0200

    Added simple init script for prediction

--- evaluate_model.sh
+++ evaluate_model.sh
@@ -1,1 +1,5 @@
-python main.py --video_root ./videos --output ./output.json --model ./resnext-101-kinetics.pth --mode score --model_name resnext --model_depth 101 --resnet_shortcut B
+#!/usr/bin/env bash
+
+videos_folder=$1
+
+python main.py --video_root videos_folder --output ./output.json --model ./resnext-101-kinetics.pth --mode score --model_name resnext --model_depth 101 --resnet_shortcut B
