990d51a9550e3203d7ad279b07a70a584bcc7e13
Eugenio <eugenio.liso@gmail.com>
2019-10-13 13:30:34 +0200

    Added limit on y_axis and added values on bar chart

--- benchmark/metrics/plot_metrics.py
+++ benchmark/metrics/plot_metrics.py
@@ -2,9 +2,11 @@ import csv
 
 import matplotlib.pyplot as plt
 import numpy as np
-from opts_metrics_plot import parse_opts_metrics_plot
 import seaborn as sns
+from opts_metrics_plot import parse_opts_metrics_plot
+
 sns.set()
+
 
 # from matplotlib.ticker import FormatStrFormatter
 
@@ -17,12 +19,27 @@ def get_metric(class_names, idx_metrics, row):
     return result, idx_metrics
 
 
-def build_plot(idx_chart, classes_metric, class_names, x_axis, title, mean_prediction_time=None, mean_accuracy=None, std_prediction_time=None):
-    _ = plt.subplot(idx_chart)
+def insert_values_on_bars(ax, bars):
+    """Attach a text label above each bar in *bars*, displaying its height."""
+    for rect in bars:
+        height = rect.get_height()
+        ax.annotate('{:.3f}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 2),
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+
+def build_plot(idx_chart, classes_metric, class_names, x_axis, title, mean_prediction_time=None, mean_accuracy=None,
+               std_prediction_time=None):
+    ax = plt.subplot(idx_chart)
     # ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
 
     barlist = plt.bar(x_axis, classes_metric)
+    insert_values_on_bars(ax, barlist)
+
     plt.xticks(x_axis, class_names)
+    plt.ylim(top=1)
 
     # Think of a general way to actually change colors dynamically
     if len(classes_metric) == 3:
@@ -31,7 +48,8 @@ def build_plot(idx_chart, classes_metric, class_names, x_axis, title, mean_predi
         barlist[2].set_color('r')
 
     if mean_prediction_time is not None and mean_accuracy is not None:
-        plt.xlabel(f'\nMean prediction time: {mean_prediction_time} - STD prediction time: {std_prediction_time} - Accuracy: {mean_accuracy}')
+        plt.xlabel(
+            f'\nMean prediction time: {mean_prediction_time} - STD prediction time: {std_prediction_time} - Accuracy: {mean_accuracy}')
 
     plt.title(title)
 
@@ -73,7 +91,8 @@ if __name__ == '__main__':
                 mean_accuracy = '{:.4f}'.format(float(mean_accuracy))
 
                 build_plot(131, classes_precision, class_names, x_axis, "Classes Precision")
-                build_plot(132, classes_recall, class_names, x_axis, "Classes Recall", mean_prediction_time, mean_accuracy, std_prediction_time)
+                build_plot(132, classes_recall, class_names, x_axis, "Classes Recall", mean_prediction_time,
+                           mean_accuracy, std_prediction_time)
                 build_plot(133, classes_fscore, class_names, x_axis, "Classes F-Score")
 
                 plt.subplots_adjust(wspace=0.5, hspace=1)
