923a4d4f430846bc90e73c7292ec84732f6ed2a1
Eugenio <eugenio.liso@gmail.com>
2019-10-20 13:24:31 +0200

    Added hint for future work

--- dataset.py
+++ dataset.py
@@ -3,6 +3,8 @@ from datasets.videodataset_multiclips import (VideoDatasetMultiClips,
                                               collate_fn)
 from pathlib import Path
 from os.path import join
+
+
 def get_validation_data(video_path,
                         annotation_path,
                         spatial_transform=None,
@@ -12,6 +14,9 @@ def get_validation_data(video_path,
     video_path_formatter = (
         lambda root_path, label, video_id: Path(join(root_path, label, video_id)))
 
+    # TODO allow a more general way to specify validation or test
+    # Right now, with validation we mean 'test set', so if in training you have
+    # a 'real' validation set, this won't work, since you should load data with subset 'testing'
     validation_data = VideoDatasetMultiClips(
         video_path,
         annotation_path,
